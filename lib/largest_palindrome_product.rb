b3e85bc9899bd6948d2c10d8e41d1d624a94f1a9
jchamberlain909 <jchamberlain909@gmail.com>
2018-10-14 21:58:30 -0400

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,27 @@
 # Implement your procedural solution here!
+
+def is_palindrome? num 
+    num_as_str = num.to_s
+    i=0
+    while i<num_as_str.length/2
+        if num_as_str[i]!=num_as_str[num_as_str.length-1-i]
+            return false    
+        end
+        i+=1
+    end
+    true   
+end
+
+def largest_palindrome_product
+    largest = 0
+
+    (100..999).each do |i|
+        (100..999).each do |j|
+            product = i*j
+            if is_palindrome?(product) && product > largest
+                largest = product
+            end
+        end 
+    end 
+    largest
+end
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,30 @@
 # Implement your object-oriented solution here!
+class LargestPalindromeProduct
+    def answer
+        largest = 0
+
+        (100..999).each do |i|
+            (100..999).each do |j|
+                product = i*j
+                if self.is_palindrome?(product) && product > largest
+                    largest = product
+                end
+            end 
+        end 
+        largest
+    end
+
+    def is_palindrome? num 
+        num_as_str = num.to_s
+        i=0
+        while i<num_as_str.length/2
+            if num_as_str[i]!=num_as_str[num_as_str.length-1-i]
+                return false    
+            end
+            i+=1
+        end
+        true   
+    end
+    
+    
+end
